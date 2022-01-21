444b1c92fd04dce9054a9e7ecc31d2f73a276f79
Divyanshu <71317784+divyanshucs052@users.noreply.github.com>
2022-01-21 22:31:05 +0530

    Create case.sh

---
+++ lab6/case.sh
@@ -0,0 +1,26 @@
+#! /bin/sh
+
+echo "Enter the alphabet"
+read char
+
+case $char in
+
+	'a') echo "Vowel"
+	;;
+
+	'e') echo "Vowel"
+	;;
+
+	'i') echo "Vowel"
+	;;
+
+	'o') echo "Vowel"
+	;;
+
+	'u') echo "Vowel"
+	;;
+
+	*) echo "Consonant"
+	;;
+
+esac
