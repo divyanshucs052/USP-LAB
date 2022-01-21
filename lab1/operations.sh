2829f701aa431b672f3a8e1f1667ce1729a07956
Divyanshu <71317784+divyanshucs052@users.noreply.github.com>
2022-01-21 22:26:51 +0530

    Create operations.sh

---
+++ lab1/operations.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+
+echo "Enter first number"
+read a
+echo "Enter second number"
+read b
+sum=`echo $a + $b|bc`
+product=`echo $a \*$b|bc`
+difference=`echo $a - $b|bc`
+mod=`echo $a%$b|bc`
+quotient=`echo $a/$b|bc`
+echo "Sum is  "$sum
+echo "Product is  "$product
+echo "Difference is  "$difference
+echo "Mod is  "$mod
+echo "Quotient is  "$quotient
+© 2022 GitHub, Inc.
+Terms
